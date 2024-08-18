e7ffd46fa6cd9d3f4bf883073093b8ae3f000fca
jojopko <gamerat.github@gmail.com>
2024-08-18 09:43:26 +0300

    Добавил поддержку Minio S3 Storage

--- api/app.py
+++ api/app.py
@@ -1,7 +1,14 @@
 import fastapi
 import pydantic
+from minio import Minio
 
 app = fastapi.FastAPI()
+
+client = Minio("s3-storage:9000",
+               access_key="Y4V9npKFwGru81ZDASq8",
+               secret_key="D9ZG56RepFWdLzVVJz4hbSGEhsaySI8Hc1LuiwMW",
+               cert_check=False,
+               secure=False)
 
 
 class Video(pydantic.BaseModel):
@@ -12,6 +19,8 @@ class Video(pydantic.BaseModel):
 
 @app.get('/health-check')
 async def health_check():
+    if not client.bucket_exists('enviloup'):
+        raise fastapi.exceptions.HTTPException(status_code=404, detail='`enviloup` S3 bucket is not exist!')
     return {'status': 'ok'}
 
 
@@ -23,6 +32,21 @@ VIDEOS_DATABASE = [
           title='The concept meaning',
           descrition='The concept meaning by Nikita'),
     Video(id=3,
+          title='What is Enviloup?',
+          descrition='Video about Enviloup'),
+    Video(id=4,
+          title='What is Enviloup?',
+          descrition='Video about Enviloup'),
+    Video(id=5,
+          title='What is Enviloup?',
+          descrition='Video about Enviloup'),
+    Video(id=6,
+          title='What is Enviloup?',
+          descrition='Video about Enviloup'),
+    Video(id=7,
+          title='What is Enviloup?',
+          descrition='Video about Enviloup'),
+    Video(id=8,
           title='What is Enviloup?',
           descrition='Video about Enviloup'),
 ]
